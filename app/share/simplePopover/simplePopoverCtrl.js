82c8c0aebc840298f538028f4efc8720a24140dc
ildar <mingazov.i@etton.ru>
2016-10-05 13:32:05 +0300

    Add new directive simplePopover

---
+++ app/share/simplePopover/simplePopoverCtrl.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+  angular
+  .module('BIONApp')
+  .controller('simplePopoverCtrl', ['$scope', '$state', simplePopoverCtrl]);
+
+  function simplePopoverCtrl($scope, $state) {
+    console.log('simplePopoverCtrl');
+
+
+  }
+
+})();
---
+++ app/share/simplePopover/simplePopoverDrtv.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+  angular
+  .module('BIONApp')
+  .directive('simplePopover', simplePopover);
+
+  function simplePopover() {
+    return {
+      scope: {
+        name: '@',
+        title: '@',
+        placement: '@',
+        toggleName: '@',
+        content: '=?'
+      },
+      restrict: 'E',
+      controller: 'simplePopoverCtrl',
+      templateUrl: 'share/simplePopover/simplePopoverTmpl.html',
+      link: function(scope, element, attrs){
+            $('[data-toggle="'+scope.toggleName+'"]').popover({
+              	html: true,
+                title: scope.title,
+                placement: scope.placement,
+                content: function() {
+                      return $('.'+scope.toggleName+'-'+scope.name).html();
+                    }
+              });
+  			}
+    };
+  }
+})();
