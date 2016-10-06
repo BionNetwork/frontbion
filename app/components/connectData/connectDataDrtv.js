3e4a41af584a269d36b0663f02b901d3833a04f3
ildar <mingazov.i@etton.ru>
2016-10-06 15:33:11 +0300

    Fix some bugs in arguments

--- app/components/connectData/connectDataDrtv.js
+++ app/components/connectData/connectDataDrtv.js
@@ -8,7 +8,8 @@
     return {
       scope: {
         allArguments: '=?',
-        cardKey: '=?'
+        cardKey: '=?',
+        parentUrl: '@'
       },
       restrict: 'E',
       controller: 'connectDataCtrl',
