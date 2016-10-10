38b24596530060ad25071bcb22f74a67915e66fc
Рустам Идрисов <idrisov.r@etton.ru>
2016-10-10 15:56:30 +0300

    fix style mainDashboard+

--- app/routes/route.js
+++ app/routes/route.js
@@ -12,25 +12,25 @@
         templateUrl: 'main.html',
         controller: ['$scope', '$http', function($scope, $http) {
           $scope.token = window.localStorage.getItem('token');
-          // var cards = {
-          //   get: {
-          //     success: function(response) {
-          //       $scope.oneOne = response.data.data;
-          //     },
-          //     error: function(response) {
-          //       console.log(response, 'error');
-          //       window.location.hash = '/login';
-          //     }
-          //   }
-          // };
-          //
-          // $http({
-          //   method: 'GET',
-          //   url: '/api/v1/cards',
-          //   headers: {
-          //     'X-AUTHORIZE-TOKEN': $scope.token
-          //   }
-          // }).then(cards.get.success, cards.get.error);
+          $scope.language = window.localStorage.getItem('lang') ? window.localStorage.getItem('lang') : 'ru';
+          var strings = {
+            get: {
+              success: function(response) {
+                $scope.allStrings = response.data.data;
+              },
+              error: function(response) {
+              }
+            }
+          };
+
+          $http({
+            method: 'GET',
+            url: '/api/v1/config/strings',
+            headers: {
+              'X-AUTHORIZE-TOKEN': $scope.token,
+              'Accept-Language' : $scope.language
+            }
+          }).then(strings.get.success, strings.get.error);
         }]
       })
       .state('not-found', {
