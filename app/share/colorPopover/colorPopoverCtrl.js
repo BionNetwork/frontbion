68a6cba3fe719e3825970f597305520c12c9d24a
ildar <mingazov.i@etton.ru>
2016-10-11 17:41:04 +0300

    Color popover integration

--- app/share/colorPopover/colorPopoverCtrl.js
+++ app/share/colorPopover/colorPopoverCtrl.js
@@ -6,7 +6,34 @@
 
   function colorPopoverCtrl($scope, $state) {
     // console.log('colorPopoverCtrl');
+    $scope.color = {
+      "background-color" : 'red'
+    }
 
+    $scope.chooseColorsObj = [
+      {name: 'gentle-pink', color: '#f15347'},
+      {name: 'chritsmas-tree', color: '#1fa89e'},
+      {name: 'pink', color: '#e24187'},
+      {name: 'grass', color: '#6bb76c'},
+      {name: 'green', color: '#9ccd64'},
+      {name: 'purple', color: '#b540c5'},
+      {name: 'violet', color: '#7e58bd'},
+      {name: 'gentle-green', color: '#d7e246'},
+      {name: 'night', color: '#5c6db2'},
+      {name: 'yellow current', color: '#fdf054'},
+      {name: 'sky', color: '#48a5ea'},
+      {name: 'orange', color: '#f6cc22'},
+      {name: 'blue', color: '#21bbef'},
+      {name: 'mandarin', color: '#ffa42c'},
+      {name: 'cyanogen', color: '#2fc5c3'},
+      {name: 'sunrise', color: '#fa7147'},
+    ];
+
+    $scope.chooseColors = function (color) {
+      $scope.color = {
+        "background-color" : color
+      }
+    };
 
   }
 
