9bcd1f1cbf09cbe656d3c1b10b1f26dfae911a7d
ildar <mingazov.i@etton.ru>
2016-10-11 17:13:03 +0300

    Prevent default login page

--- app/share/loginPage/loginPageCtrl.js
+++ app/share/loginPage/loginPageCtrl.js
@@ -8,6 +8,7 @@
     // console.log('loginPageCtrl');
 
     $scope.onLoginPage = function (user, pass) {
+      event.preventDefault();
       if (user && pass) {
         $scope.loginPage(user, pass);
       }
