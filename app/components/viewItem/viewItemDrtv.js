d4a73051c7cfc81e4f0b0bd1509ea41d93b058c8
Рустам Идрисов <idrisov.r@etton.ru>
2016-10-12 16:58:07 +0300

    header big title

--- app/components/viewItem/viewItemDrtv.js
+++ app/components/viewItem/viewItemDrtv.js
@@ -14,7 +14,15 @@
       controller: 'viewItemCtrl',
       templateUrl: 'components/viewItem/viewItemTmpl.html',
       link: function (scope, element, attr) {
-          $('.slick-slider').slick();
+          $('.slick-slider').slick({
+            dots: true,
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear',
+            slidesToShow: 1,
+            adaptiveHeight: true
+          });
           // element[0].querySelector('.your-class').slick();
       }
     };
