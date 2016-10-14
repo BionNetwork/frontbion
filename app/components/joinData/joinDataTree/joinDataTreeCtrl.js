d170553ae6e4d22d521f592d0b04782a6786522a
ildar <mingazov.i@etton.ru>
2016-10-14 12:05:37 +0300

    Run tree++++

--- app/components/joinData/joinDataTree/joinDataTreeCtrl.js
+++ app/components/joinData/joinDataTree/joinDataTreeCtrl.js
@@ -5,13 +5,18 @@
   .controller('joinDataTreeCtrl', ['$scope', '$state', '$timeout', joinDataTreeCtrl]);
 
   function joinDataTreeCtrl($scope, $state, $timeout) {
-    
+
       $(document).ready(function(){
+            $('.event').on("dragstart", function (event) {
+        			  var dt = event.originalEvent.dataTransfer;
+        			  dt.setData('Text', $(this).attr('id'));
+        			});
       	    $('table td').on("dragenter dragover drop", function (event) {
       		   event.preventDefault();
       		   if (event.type === 'drop') {
       			  var data = event.originalEvent.dataTransfer.getData('Text',$(this).attr('id'));
-              var an = $('#a').detach();
+              var an = data ? $('#' + data).detach() : $('#a').detach();
+              // var an = $('#a').detach();
       			  // var de=$('#'+data).detach();
 
       			  an.appendTo($(this));
