14d7252ea4697478073fd605dc934bfd81d4cd8a
ildar <mingazov.i@etton.ru>
2016-10-15 19:31:59 +0300

    Change currency+

--- app/components/showCard/showCardCtrl.js
+++ app/components/showCard/showCardCtrl.js
@@ -97,15 +97,27 @@
                  data: data.map(row => row[$scope.data.series.x.index])
              }
          };
-            $scope.onFilterChartPie(newDataForChart);
-            $scope.onFilterChartLine(newDataForChart);
-            $scope.onFilterChartColumn(newDataForChart);
+         if ($scope.chart == 'pie') {
+           $scope.onFilterChartPie(newDataForChart);
+         }
+         if ($scope.chart == 'line') {
+           $scope.onFilterChartLine(newDataForChart);
+         }
+         if ($scope.chart == 'column') {
+           $scope.onFilterChartColumn(newDataForChart);
+         }
       };
       if (filter == 'null') {
         var initalState = $scope.filterData($scope.data.series, $scope.data.data);
-          $scope.onFilterChartPie(initalState);
-          $scope.onFilterChartLine(initalState);
-          $scope.onFilterChartColumn(initalState);
+          if ($scope.chart == 'pie') {
+            $scope.onFilterChartPie(initalState);
+          }
+          if ($scope.chart == 'line') {
+            $scope.onFilterChartLine(initalState);
+          }
+          if ($scope.chart == 'column') {
+            $scope.onFilterChartColumn(initalState);
+          }
       }
     };
 
