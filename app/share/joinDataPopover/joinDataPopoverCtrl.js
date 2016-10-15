82aa52b6f29609b3ca8f0d87c939527bd839190b
ildar <mingazov.i@etton.ru>
2016-10-15 20:12:36 +0300

    Fix Translates+

--- app/share/joinDataPopover/joinDataPopoverCtrl.js
+++ app/share/joinDataPopover/joinDataPopoverCtrl.js
@@ -7,6 +7,8 @@
   function joinDataPopoverCtrl($scope, $state) {
         $scope.popoverHeadName = "Choose";
         $scope.popoverHeadShow = false;
+        $scope.stringJoin = window.localStorage.getItem('lang') == 'en' ? "JOIN" : "СВЯЗАТЬ";
+        $scope.stringTitle = window.localStorage.getItem('lang') == 'en' ? "Tables" : "Таблицы";
 
 
         $scope.changePopoverHead = function (item) {
