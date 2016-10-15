561f4f58ecb46d4dac8826eb9457bd2f301caa62
ildar <mingazov.i@etton.ru>
2016-10-15 17:35:15 +0300

    Translate login page

--- app/share/loginPage/loginPageCtrl.js
+++ app/share/loginPage/loginPageCtrl.js
@@ -12,11 +12,42 @@
       if (user && pass) {
         $scope.loginPage(user, pass);
       }
+      if (!window.localStorage.getItem('lang')) {
+        window.localStorage.setItem("lang", 'ru');
+      }
+
     };
 
     $scope.contentLang = [
         {name: "Русский"}
     ];
+
+    $scope.changeLang = function (item) {
+      window.localStorage.setItem("lang", item);
+      if (item == 'en') {
+        $scope.string = {
+          onEnter: 'To login enter your username and password',
+          login: 'Your login',
+          pass: 'Your password',
+          enter: 'Log in',
+        };
+      }
+      if (item == 'ru') {
+        $scope.string = {
+          onEnter: 'Для входа укажите ваш логин и пароль',
+          login: 'Ваш логин',
+          pass: 'Ваш пароль',
+          enter: 'Войти',
+        };
+      }
+    };
+
+    $scope.string = {
+      onEnter: 'Для входа укажите ваш логин и пароль',
+      login: 'Ваш логин',
+      pass: 'Ваш пароль',
+      enter: 'Войти',
+    };
 
 
 
