d1d66b4e65cd9ba22142908abebeaa4808d95957
ildar <mingazov.i@etton.ru>
2016-10-18 10:00:49 +0300

    Categorizing in myWidget page

--- app/components/myWidgets/myWidgetsCtrl.js
+++ app/components/myWidgets/myWidgetsCtrl.js
@@ -9,34 +9,45 @@
     $scope.idEnglish = window.localStorage.getItem('lang') == 'en' ? false : true;
 
     $scope.getCategoryOfCard = function (cards, id) {
-      console.log(id);
+      if (id == 0 && cards) {
+        var newCards = cards.filter(function (item) {
+          return (item.card.id == 0) ;
+        });
+        return newCards;
+      }
       if (id == 1 && cards) {
         var newCards = cards.filter(function (item) {
-          return (item.id == 13 || item.id == 6 || item.id == 12 || item.id == 8 || item.id == 24) ;
+          return (item.card.id == 13 || item.card.id == 6 || item.card.id == 12 || item.card.id == 8 || item.card.id == 24) ;
         });
         return newCards;
       }
       if (id == 2 && cards) {
         var newCards = cards.filter(function (item) {
-          return (item.id == 14 || item.id == 15 || item.id == 17 || item.id == 18 || item.id == 19  || item.id == 22) ;
+          return (item.card.id == 14 || item.card.id == 15 || item.card.id == 17 || item.card.id == 18 || item.card.id == 19  || item.card.id == 22) ;
         });
         return newCards;
       }
       if (id == 3 && cards) {
         var newCards = cards.filter(function (item) {
-          return (item.id == 9 || item.id == 20) ;
+          return (item.card.id == 9 || item.card.id == 20) ;
         });
         return newCards;
       }
       if (id == 4 && cards) {
         var newCards = cards.filter(function (item) {
-          return (item.id == 7 || item.id == 16) ;
+          return (item.card.id == 7 || item.card.id == 16) ;
         });
         return newCards;
       }
       if (id == 5 && cards) {
         var newCards = cards.filter(function (item) {
-          return (item.id == 11 || item.id == 10) ;
+          return (item.card.id == 11 || item.card.id == 10) ;
+        });
+        return newCards;
+      }
+      if (id == 6 && cards) {
+        var newCards = cards.filter(function (item) {
+          return (item.card.id == 0) ;
         });
         return newCards;
       }
